b11b34c56035c43c0b615d5e552b842c095103fa
Kristian-045 <kaleta.kristian@gmail.com>
2020-09-05 14:07:44 +0200

    uprava css s oznacenim kristian pridanie icon do app.blade a uprava login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,64 +7,42 @@
             <div class="card   border-form ">
                 <div class="card-header border-primary border-width text-center">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                    <div class="card-body">
 
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                            <div class="border-custom mb-3">
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback d-block text-center mb-3" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                                    @enderror
+                                <div class="input-group  ">
+                                    <div class="input-group-append ">
+                                        <span class="input-group-text border-0 "><i class="fas fa-user"></i></span>
+                                    </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                    <input type="email" name="email" class="text-center container-fluid  border-0 p-1 bg-transparent  @error('email') is-invalid @enderror" placeholder="Email..." class="form-control">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
                                 </div>
                             </div>
-                        </div>
+                            <div class="border-custom ">
+                                @error('password')
+                                    <span class="invalid-feedback mb-3" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                <div class="input-group  ">
+                                    <div class="input-group-append ">
+                                        <span class="input-group-text border-0 "><i class="fas fa-user"></i></span>
+                                    </div>
+                                    <input type="password" name="password" class="text-center container-fluid  border-0 p-1 bg-transparent  @error('password') is-invalid @enderror" placeholder="Password..." class="form-control">
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                </div>
                             </div>
-                        </div>
+
+                        <button class="container-fluid btn btn-primary  mt-4" value="add" type="submit">{{__('Login')}}</button>
                     </form>
                     @guest
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <!-- Font awesome ... icons -->
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
